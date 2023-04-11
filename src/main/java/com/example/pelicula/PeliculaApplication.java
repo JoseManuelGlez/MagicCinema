67db8d8be2a57ffc0ac2aa7b2fc5eeea47815fb1
JoseManuelGlez <213505@ids.upchiapas.edu.mx>
2023-04-11 00:57:32 -0600

    cors

--- src/main/java/com/example/pelicula/PeliculaApplication.java
+++ src/main/java/com/example/pelicula/PeliculaApplication.java
@@ -2,12 +2,22 @@ package com.example.pelicula;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 @SpringBootApplication
-public class PeliculaApplication {
+public class PeliculaApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(PeliculaApplication.class, args);
     }
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 }
